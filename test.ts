80fe89c414c40724695c5b9f0d60fcbe99d16f5f
Alex.hu <huzhongyuan@gmail.com>
2023-06-24 15:23:59 +0800

    更新 pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,9 @@
 // 在此处测试；当此软件包作为插件使用时，将不会编译此软件包。
 // 在此处添加您的代码
-for (let i = 0; i < 25; i++) {
-    screenMagic.plotAt(i)
-    basic.pause(100)
+while(true) {
+    for (let i = 0; i < 25; i++) {
+        screenMagic.plotAt(i)
+        basic.pause(100)
+    }
+    basic.clearScreen()
 }
